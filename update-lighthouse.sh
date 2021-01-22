6f36de8bf1904fe73316d6d993ca16f4daa437dd
etimoz <etimoz@users.noreply.github.com>
2021-01-22 16:34:41 +0100

    only pull relevant images

--- update-lighthouse.sh
+++ update-lighthouse.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker-compose pull
+docker-compose pull beacon_node validator_client
 # stop and remove containers
 yes | docker-compose rm -s -v beacon_node validator_client
 docker-compose up -d beacon_node validator_client
