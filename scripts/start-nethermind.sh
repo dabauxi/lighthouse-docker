ee9544c7167d8743a1f18ab7b50aa22b190bc0c7
dabauxi <dabauxi@users.noreply.github.com>
2022-03-19 23:20:05 +0100

    Only enable eth and net rpc module

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local nethermind node.
 
-cd /nethermind && ./Nethermind.Runner --JsonRpc.Enabled true --Pruning.Mode "Full" --JsonRpc.Host 0.0.0.0 --datadir data
+cd /nethermind && ./Nethermind.Runner --JsonRpc.Enabled true  --JsonRpc.EnabledModules "Eth,Net" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
