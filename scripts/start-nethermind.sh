a0a2ee019a4858de2d9cf05b83fd170f4580aa4c
dabauxi <dabauxi@users.noreply.github.com>
2022-03-19 23:07:58 +0100

    Set rpc host 0.0.0.0, Add datadir arg

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local nethermind node.
 
-cd /nethermind && ./Nethermind.Runner --JsonRpc.Enabled true --Pruning.Mode "Full"
+cd /nethermind && ./Nethermind.Runner --JsonRpc.Enabled true --Pruning.Mode "Full" --JsonRpc.Host 0.0.0.0 --datadir data
