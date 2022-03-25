722b221a629483e32fc921874bce0aacc762021c
dabauxi <dabauxi@users.noreply.github.com>
2022-03-25 07:52:57 +0100

    Add cache value for geth

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -12,10 +12,16 @@ if [ "$DISABLE_SNAPSHOT" != "" ]; then
 	SNAPSHOT_FLAG="--snapshot=false"
 fi
 
+
+if [ "$CACHE" != "" ]; then
+	CACHE="--cache $CACHE"
+fi
+
+
 if [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $SNAPSHOT_FLAG
+		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $SNAPSHOT_FLAG $CACHE
 	else
-		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $SNAPSHOT_FLAG
+		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $SNAPSHOT_FLAG $CACHE
 	fi
 fi
