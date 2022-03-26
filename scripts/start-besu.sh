2e046796d1b95ece4af08d7b6826255bff027b74
dabauxi <dabauxi@users.noreply.github.com>
2022-03-26 15:09:51 +0100

    Enable bonsai tries

--- scripts/start-besu.sh
+++ scripts/start-besu.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local fast-synced geth node.
 
-/opt/besu/bin/besu --network=mainnet --rpc-http-enabled --rpc-http-host=0.0.0.0 --host-allowlist=* --data-path=/var/lib/besu --Xdata-storage-format=BONSAI
+/opt/besu/bin/besu --Xdata-storage-format=BONSAI --Xbonsai-maximum-back-layers-to-load=500 --network=mainnet --rpc-http-enabled --rpc-http-host=0.0.0.0 --host-allowlist=* --data-path=/var/lib/besu
