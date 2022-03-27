d236fe2837e046dbe0e1b6de8dd7f01c206b7c3a
dabauxi <dabauxi@users.noreply.github.com>
2022-03-27 17:53:09 +0200

    Set network to mainnet

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local nethermind node.
 
-./Nethermind.Runner --config mainnet_pruned --JsonRpc.Enabled true  --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
+./Nethermind.Runner --config mainnet --JsonRpc.Enabled true  --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
