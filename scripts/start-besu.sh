f31f55c546fd674549b3a104da8441e47271715a
dabauxi <dabauxi@users.noreply.github.com>
2022-04-02 11:08:18 +0200

    Change script set environment

--- scripts/start-besu.sh
+++ scripts/start-besu.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local fast-synced geth node.
 
-/opt/besu/bin/besu --network=mainnet --rpc-http-enabled --rpc-http-api "WEB3,ETH,NET"--rpc-http-host=0.0.0.0 --rpc-http-cors-origins=* --host-allowlist=* --data-path=/var/lib/besu
+exec besu --network=mainnet --rpc-http-enabled --rpc-http-host=0.0.0.0 --rpc-http-cors-origins=* --host-allowlist=* --data-path=/var/lib/besu
