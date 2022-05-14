1185f7fd4151a6f86ea5e3317ac71f04954037dc
dabauxi <dabauxi@users.noreply.github.com>
2022-05-14 21:38:49 +0200

    Set snap sync

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local nethermind node.
 
-./Nethermind.Runner --config mainnet --JsonRpc.Enabled true  --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
+./Nethermind.Runner --config mainnet --Sync.SnapSync true --JsonRpc.Enabled true --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
