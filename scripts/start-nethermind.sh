eb2dc7eb9e688197303412bdc6effdd59c96bccf
dabauxi <dabauxi@users.noreply.github.com>
2022-05-15 19:35:59 +0200

    Add additional nethermind start options

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,4 @@
 #
 # Starts a local nethermind node.
 
-./Nethermind.Runner --config mainnet --Sync.SnapSync true --JsonRpc.Enabled true --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full"
+./Nethermind.Runner --config mainnet --Sync.SnapSync true --JsonRpc.Enabled true --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full" --Pruning.CacheMb "4096" --Sync.AncientBodiesBarrier "11052984" --Sync.AncientReceiptsBarrier "11052984" --Sync.DownloadBodiesInFastSync true --Sync.DownloadReceiptsInFastSync true
