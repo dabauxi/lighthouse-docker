ddcc89f8d0ef930f721446986b188b4e2a2910a4
dabauxi <dabauxi@users.noreply.github.com>
2022-05-15 19:42:04 +0200

    Add higher kill timeout

--- update-nethermind.sh
+++ update-nethermind.sh
@@ -2,5 +2,5 @@
 
 docker-compose pull nethermind
 # stop and remove containers
-yes | docker-compose rm -s -v nethermind
+docker-compose stop -t 60 nethermind && yes | docker-compose rm -v nethermind
 docker-compose up -d nethermind
