9f9762620581814b03ba6ebc05fc2a0c0d624665
dabauxi <dabauxi@users.noreply.github.com>
2022-07-03 20:47:30 +0200

    Add start, stop script, Increase stop timeout

--- update-lighthouse.sh
+++ update-lighthouse.sh
@@ -2,5 +2,5 @@
 
 docker-compose pull beacon_node validator_client
 # stop and remove containers
-docker-compose stop -t 60 beacon_node validator_client && yes | docker-compose rm -v beacon_node validator_client
+docker-compose stop -t 180 beacon_node validator_client && yes | docker-compose rm -v beacon_node validator_client
 docker-compose up -d beacon_node validator_client
