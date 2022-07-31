03b9729fc04a8745d4c8d6dbcc0ab5a238aa6d1a
dabauxi <dabauxi@users.noreply.github.com>
2022-07-31 09:30:09 +0200

    Changed config

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,19 +2,22 @@
 #
 # Starts a local nethermind node.
 
-./Nethermind.Runner \
-  --config mainnet  \
-  --Sync.SnapSync true \
-  --JsonRpc.Enabled true \
-  --JsonRpc.EnabledModules "Admin,Web3,Eth,Subscribe,Net" \
-  --JsonRpc.Host 0.0.0.0 \
-  --JsonRpc.Port 8545 \
-  --datadir data \
-  --Pruning.Mode "Full" \
-  --Pruning.CacheMb "4096" \
-  --Sync.AncientBodiesBarrier "11052984" \
-  --Sync.AncientReceiptsBarrier "11052984" 
-  --Sync.DownloadBodiesInFastSync true \
-  --Sync.DownloadReceiptsInFastSync true \
-  --HealthChecks.Enabled true \
-  --HealthChecks.*
+exec ./Nethermind.Runner \
+    --config mainnet \
+    --Sync.SnapSync true \
+    --JsonRpc.Enabled true \
+    --JsonRpc.EnabledModules "Admin,Web3,Eth,Subscribe,Net" \
+    --JsonRpc.Host 0.0.0.0 \
+    --JsonRpc.Port 8545 \
+    --JsonRpc.JwtSecretFile="/secrets/jwtsecret" \
+    --Init.WebSocketsEnabled true \
+    --JsonRpc.WebSocketsPort 8546 \
+    --datadir data \
+    --Pruning.Mode "Full" 
+    --Pruning.CacheMb "4096" \
+    --Sync.AncientBodiesBarrier "11052984" \
+    --Sync.AncientReceiptsBarrier "11052984" \
+    --Sync.DownloadBodiesInFastSync true \
+    --Sync.DownloadReceiptsInFastSync true \
+    --HealthChecks.Enabled true \
+    --HealthChecks.*
