5a40dfb53050257380d2f49529dbd4ad74f6e017
dabauxi <dabauxi@users.noreply.github.com>
2022-07-31 09:07:28 +0200

    Format script

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,4 +2,18 @@
 #
 # Starts a local nethermind node.
 
-./Nethermind.Runner --config mainnet --Sync.SnapSync true --JsonRpc.Enabled true --JsonRpc.EnabledModules "Eth,Net,Admin" --JsonRpc.Host 0.0.0.0 --datadir data --Pruning.Mode "Full" --Pruning.CacheMb "4096" --Sync.AncientBodiesBarrier "11052984" --Sync.AncientReceiptsBarrier "11052984" --Sync.DownloadBodiesInFastSync true --Sync.DownloadReceiptsInFastSync true --HealthChecks.Enabled true
+./Nethermind.Runner \
+  --config mainnet  \
+  --Sync.SnapSync true \
+  --JsonRpc.Enabled true \
+  --JsonRpc.EnabledModules "Admin,Web3,Eth,Subscribe,Net" \
+  --JsonRpc.Host 0.0.0.0 \
+  --JsonRpc.Port 8545 \
+  --datadir data \
+  --Pruning.Mode "Full" \
+  --Pruning.CacheMb "4096" \
+  --Sync.AncientBodiesBarrier "11052984" \
+  --Sync.AncientReceiptsBarrier "11052984" 
+  --Sync.DownloadBodiesInFastSync true \
+  --Sync.DownloadReceiptsInFastSync true \
+  --HealthChecks.Enabled true
