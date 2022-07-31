21e79f0d0a59b7531a8a5538630c9ac8b92d949c
dabauxi <dabauxi@users.noreply.github.com>
2022-07-31 21:54:06 +0200

    Move comment

--- scripts/start-nethermind.sh
+++ scripts/start-nethermind.sh
@@ -2,6 +2,9 @@
 #
 # Starts a local nethermind node.
 
+#--JsonRpc.AdditionalRpcUrls="http://0.0.0.0:8551|http;ws|engine;eth;subscribe" \
+#--JsonRpc.JwtSecretFile="/secrets/jwtsecret" \
+
 exec ./Nethermind.Runner \
     --config mainnet \
     --Sync.SnapSync true \
@@ -9,8 +12,6 @@ exec ./Nethermind.Runner \
     --JsonRpc.EnabledModules "Admin,Web3,Eth,Subscribe,Net" \
     --JsonRpc.Host 0.0.0.0 \
     --JsonRpc.Port 8545 \
-    #--JsonRpc.AdditionalRpcUrls="http://0.0.0.0:8551|http;ws|engine;eth;subscribe" \
-    #--JsonRpc.JwtSecretFile="/secrets/jwtsecret" \
     --Init.WebSocketsEnabled true \
     --datadir /nethermind/data \
     --Pruning.Mode "Full" \
